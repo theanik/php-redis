3e7dbd902a7d61fdc3ff9f009cfb2b597a03c5f5
theanik <anwar.anik33@gmail.com>
2020-06-15 10:10:24 +0600

    Radit connect,set,get,expire, delete

--- redis01.php
+++ redis01.php
@@ -5,13 +5,13 @@ use Predis\Client;
 
 $client = new Client();
 $client->set("name","Anik");
-$name = $client->get("name");
+$name = $client->get("name")."\n";
 
 // $client->set("name2","Anwar");
 // $client->expire("name2",30); // name2 will del after 30 secound
 
 echo $name;
-echo $client->get("name2");
+echo $client->get("name2")."\n";
 
 // Store Array in redis
 
@@ -23,4 +23,13 @@ $user1 = [
 
 $client->set("user1", json_encode($user1));
 
-echo $client->get("user1");
+echo $client->get("user1")."\n";
+
+
+// Delete from redis
+
+
+$client->set("temp", "this is tmp data");
+echo $client->get("temp");
+$client->del("temp"); // data deleted
+echo $client->get("temp"); // nouthing print
